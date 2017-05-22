f065a6b066cda54ee45c0df52ddb2d2f5f39417a
Mehul Srivastava <you@example.commehul.srivastava1@globallogic.com>
2017-05-22 17:07:06 +0530

    Added Answers

--- Retain Cycles/RetainViewController.m
+++ Retain Cycles/RetainViewController.m
@@ -44,9 +44,12 @@
     self.sampleObject = [[SampleObject alloc] init];
     self.someViewController = [[SomeViewController alloc] init];
 
+    //Which one of the following code snippets will cause a retain cycle?
     
     
     // 1.
+    // Answer: No
+    // Reason: The block is not being held with a strong reference
     dispatch_async(dispatch_get_main_queue(), ^{
         NSLog(@"Capturing Self %@", self);
     });
@@ -54,6 +57,8 @@
     
     
     // 2.
+    // Answer: No
+    // Reason: The block is not being held with a strong reference
     dispatch_async(dispatch_queue_create("com.test.app", NULL),^{
         NSLog(@"Capturing Self %@", self);
     });
@@ -61,13 +66,17 @@
     
     
     // 3.
+    // Answer: No
+    // Reason: The block is not being held with a strong reference
     [self.myArray enumerateObjectsUsingBlock:^(id x, NSUInteger index, BOOL *stop){
         NSLog(@"Capturing Self %@", self);
     }];
     
     
     
-    // 4.   
+    // 4.
+    // Answer: Yes
+    // Reason: The block is being held with a strong reference by self. The block is holding a strong reference of self.
     self.simpleBlock = ^(){
         NSLog(@"Capturing Self %@", self);
     };
@@ -75,6 +84,8 @@
     
  
     // 5.
+    // Answer: Yes
+    // Reason: The block is being held with a strong reference by self. The block is holding a strong reference of self via anotherArray variable which is strong.
     self.simpleBlock2 = ^(){
         NSLog(@"Print array %@", anotherArray);
         NSLog(@"Capturing Self %@", weakSelf);
@@ -84,6 +95,8 @@
     
     
     // 6.
+    // Answer: Yes
+    // Reason: The block is being held with a strong reference by self. The block is holding a strong reference of self via _myArray strong ivar property.
     self.simpleBlock = ^(){
         NSLog(@"Print array %@", _myArray);
         NSLog(@"Capturing Self %@", weakSelf);
@@ -92,6 +105,8 @@
     
     
     // 7.
+    // Answer: Yes
+    // Reason: The block is being held with a strong reference by self. The block is holding a strong reference of self via _myBoolValue value type which compiler links to self.
     self.simpleBlock = ^(){
         NSLog(@"Capturing Self %@", weakSelf);
         NSLog(@"Value type property", _myBoolValue);
@@ -100,6 +115,8 @@
     
   
     // 8.
+    // Answer: No
+    // Reason: The block is not being held with a strong reference
     [self.myArray enumerateObjectsUsingBlock:^(id x, NSUInteger index, BOOL *stop){
         
         NSLog(@"Capturing Self %@", weakSelf);
@@ -113,8 +130,10 @@
     
     
     // 9.
+    // Answer: Yes
+    // Reason: The block is being held with a strong reference by self. The block keeps a reference to dispatch. Dispatch has a strong reference to self.
     self.simpleBlock = ^(){
-        //NSLog(@"Print array %@", weakSelf.myArray);
+        NSLog(@"Print array %@", weakSelf.myArray);
         NSLog(@"Capturing Self %@", weakSelf);
         
         dispatch_async(dispatch_get_main_queue(), ^{
@@ -126,6 +145,8 @@
     
     
     // 10.
+    // Answer: No
+    // Reason: The block is not being held with a strong reference
     [self.sampleObject sampleMethodWithCompletion:^{
         NSLog(@"Capturing Self %@", self);
     }];
@@ -134,6 +155,8 @@
     
     
     // 11.
+    // Answer: No
+    // Reason: The block is not being held with a strong reference
     SampleObject *anotherSampleObject = [[SampleObject alloc] init];
     [anotherSampleObject sampleMethodWithCompletion:^{
         NSLog(@"Capturing Self %@", self);
@@ -143,6 +166,8 @@
     
     
     // 12.
+    // Answer: No
+    // Reason: The block is not being held with a strong reference
     [self.sampleObject sampleMethodWithCompletion:^{
         NSLog(@"Capturing Self %@", self);
         
@@ -156,6 +181,8 @@
     
     
     // 13.
+    // Answer: No
+    // Reason: The block is not being held with a strong reference
     [self.someViewController sampleMethodWithCompletion:^{
         NSLog(@"Capturing Self %@", self.someViewController);
     }];
@@ -164,6 +191,8 @@
     
     
     // 14.
+    // Answer: Yes
+    // Reason: Self has a strong referece to sample object. Sample object has a strong reference of block. Block has a strong reference of self.
     [self.sampleObject setCompletionBlock:^{
         NSLog(@"Capturing Self %@", self);
     }];
@@ -171,12 +200,16 @@
     
     
     // 15.
+    // Answer: No
+    // Reason: The block is not being held with a strong reference
     [self sampleMethodWhichRunsABlock];
     
     
     
     
     //16.
+    // Answer: No
+    // Reason: Pointer to _myArray ivar which retains self is passed as paramter. The pointer however does not retain self.
     [self sampleMethodTakingSampleParameter:_myArray];
     
 }
